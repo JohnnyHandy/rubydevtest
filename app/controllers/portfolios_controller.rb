36e6474faf6e4d10a9941ec6d3835987277d36c6
Roger Peixoto <duarterogerpeixoto@gmail.com>
2021-02-24 09:00:32 -0300

    Update and edit funfctionalityu

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -20,5 +20,21 @@ class PortfoliosController < ApplicationController
       end
       def portfolio_params
         params.require(:portfolio).permit(:title, :subtitle, :body)
-      end
+    end
+
+    def edit
+        @portfolio_item = Portfolio.find(params[:id])
+    end
+
+    def update
+        @portfolio_item = Portfolio.find(params[:id])
+        respond_to do |format|
+          if @portfolio_item.update(portfolio_params)
+            format.html { redirect_to portfolios_path, notice: "The portfolio was successfully updated." }
+          else
+            format.html { render :edit, status: :unprocessable_entity }
+          end
+        end
+    end
+
 end
