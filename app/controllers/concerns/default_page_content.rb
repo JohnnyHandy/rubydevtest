2db53678443b2ef9cb9c358d48c5c7757d2fb217
Roger Peixoto <duarterogerpeixoto@gmail.com>
2021-03-02 08:23:00 -0300

    Added navbar and added default header content

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,10 @@
+module DefaultPageContent
+    extend ActiveSupport::Concern
+    included do
+        before_action :set_page_defaults
+    end
+    def set_page_defaults
+        @page_title = "Devcamp Portfolio | My Portfolio Website"
+        @seo_keywords = "Roger Peixoto Portfolio"
+    end
+end
