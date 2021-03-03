9eda0833b271bb03ffb3a3b82a6fd6eb980d6abe
Roger Peixoto <duarterogerpeixoto@gmail.com>
2021-03-03 07:14:23 -0300

    Added before_action to set portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :update, :destroy, :show]
     layout 'portfolio'
     def index
         @portfolio_items = Portfolio.all
@@ -34,11 +35,9 @@ class PortfoliosController < ApplicationController
     end
 
     def edit
-        @portfolio_item = Portfolio.find(params[:id])
     end
 
     def update
-        @portfolio_item = Portfolio.find(params[:id])
         respond_to do |format|
           if @portfolio_item.update(portfolio_params)
             format.html { redirect_to portfolios_path, notice: "The portfolio was successfully updated." }
@@ -49,15 +48,17 @@ class PortfoliosController < ApplicationController
     end
 
     def show
-      @portfolio_item = Portfolio.find(params[:id])
     end
 
     def destroy
-      @portfolio_item = Portfolio.find(params[:id])
       @portfolio_item.destroy
       respond_to do |format|
-        format.html { redirect_to portfolios_url, notice: "Portgolio was successfully destroyed." }
+        format.html { redirect_to portfolios_url, notice: "Portfolio was successfully destroyed." }
       end
+    end
+
+    def set_portfolio_item
+      @portfolio_item = Portfolio.find(params[:id])
     end
 
 end
